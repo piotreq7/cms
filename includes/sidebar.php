1f0546259ba1ed42473a5cd9cdc937fd64c3b348
piotreq7 <45266559+piotreq7@users.noreply.github.com>
2019-02-15 20:45:47 +0100

    x
    x

---
+++ includes/sidebar.php
@@ -0,0 +1,56 @@
+            <div class="col-md-4">
+           
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>Blog Search</h4>
+                    
+                    <form action="search.php" method="post">
+                    
+                    <div class="input-group">
+                        <input name="search" type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button name ="submit" class="btn btn-default" type="submit" value="szukaj">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    </form> <!-- search form -->
+                    <!-- /.input-group -->
+                </div>
+
+                <!-- Blog Categories Well -->
+                  
+                <div class="well">
+                <?php
+                
+                $query = "SELECT * FROM categories LIMIT 4";
+                $select_categories_sidebar = mysqli_query($connection,$query);
+                
+                
+                  
+                
+                ?>
+                    <h4>Blog Categories</h4>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <ul class="list-unstyled">
+                    <?php                               
+                    while ($row=mysqli_fetch_array($select_categories_sidebar)){
+                    $cat_title = $row['cat_title'];
+                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                         } 
+                    ?>           
+                               
+                            </ul>
+                        </div>
+                        <!-- /.col-lg-6 -->
+
+                        <!-- /.col-lg-6 -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+                <!-- Side Widget Well -->
+            <?php include "includes/widget.php";?>
+
+            </div>
